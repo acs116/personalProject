956e36586098c8dbfd30f0c5c4247fbb16270e3a
Administrator <Administrator@smart20113>
2019-06-20 09:48:07 +0900

    [개발자 31기 안찬솔 2019-06-20 09:46]
    기본적인 수정 완료
    DepartmentController.java
    DepartmentMapper.java
    DepartmentMapper.xml
    DepartmentService.java
    departmentView.html

--- team01/src/main/java/ksmart31/team01/member/mapper/DepartmentMapper.java
+++ team01/src/main/java/ksmart31/team01/member/mapper/DepartmentMapper.java
@@ -7,12 +7,11 @@ import org.apache.ibatis.annotations.Mapper;
 
 import ksmart31.team01.member.vo.Department;
 import ksmart31.team01.member.vo.DepartmentJoinMember;
-
-
 @Mapper
 public interface DepartmentMapper {
 	//조직도 리스트
 	public List<Department> selectDepartmentList();
+	//department부서명과 member조직원 기본정보를 조인
 	public List<DepartmentJoinMember> selectDepartmentJoinMemberList(Map<String, Object> map);
 	
 	
--- team01/src/main/java/ksmart31/team01/member/service/DepartmentService.java
+++ team01/src/main/java/ksmart31/team01/member/service/DepartmentService.java
@@ -1,52 +1,39 @@
 package ksmart31.team01.member.service;
 
 import java.util.*;
-import java.util.List;
-
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import ksmart31.team01.member.mapper.DepartmentMapper;
 import ksmart31.team01.member.vo.Department;
 import ksmart31.team01.member.vo.DepartmentJoinMember;
-
-
-
 @Service
 @Transactional
 public class DepartmentService {
 	@Autowired
-	private DepartmentMapper departmentMapper;
-		
+	private DepartmentMapper departmentMapper;		
 	//조직도 departmentView.html
 	public Map<String, Object> getDepartmentList(String departmentName){
-		//재귀쿼리 사용한 departmentName 조회 리스트
+		//재귀쿼리 사용한 departmentName 조직도 조회 리스트
 		List<Department> recursiveList = departmentMapper.selectDepartmentList();
-		System.out.println(recursiveList +"DepartmentService getDepartmentList recursiveList");
-		
-		//department join member list
-		
-		
+		System.out.println(recursiveList +"DepartmentService getDepartmentList recursiveList");		
+		// StringUtils.countMatches() -> departmentName중에 -가 포함되어있는 갯수를 보여준다
 		int departmentNumber = StringUtils.countMatches(departmentName, "-");
-		System.out.println("departmentNumber - 몇개? : " + departmentNumber);
-				
+		System.out.println("DepartmentService getDepartmentList departmentNumber -갯수 : " + departmentNumber);
+		// replaceAll()메서드로 -가 포함된문자열을 없애준다 		
 		if(departmentName != null){	
 			departmentName = departmentName.replaceAll("-","");
-			System.out.println("departmentName - 제거 됐니? : " + departmentName);
+			System.out.println("DepartmentService getDepartmentList departmentName -제거 확인 : " + departmentName);
 		}
-		
+		// 2개 이상의 데이터를 하나로 넘기기 위해 HashMap 사용
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("departmentName", departmentName);
-		map.put("departmentNumber", departmentNumber);
-		
-		
-		List<DepartmentJoinMember> departmentJoinMemberList = departmentMapper.selectDepartmentJoinMemberList(map);
-		
+		map.put("departmentNumber", departmentNumber);		
+		//department부서명  member조직원 기본 정보 조인	
+		List<DepartmentJoinMember> departmentJoinMemberList = departmentMapper.selectDepartmentJoinMemberList(map);		
 		System.out.println(departmentJoinMemberList + "DepartmentService getDepartmentList departmentJoinMemberList");
-		
-		
+		// 2개 이상의 데이터를 하나로 넘기기 위해 HashMap 사용	
 		Map<String, Object> returnMap = new HashMap<String, Object>();
 		returnMap.put("recursiveList", recursiveList);		
 		returnMap.put("departmentJoinMemberList", departmentJoinMemberList);
